81a699c82de668d2391a2dcd9429caa52decb4ba
NicoVill <o_sl_f@hotmail.com>
2015-09-02 11:15:29 +0200

    Create 1st Version of GUI

--- AuctionHouseClient/MainWindow.xaml.cs
+++ AuctionHouseClient/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace AuctionHouseClient
         {
             InitializeComponent();
         }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
     }
 }
